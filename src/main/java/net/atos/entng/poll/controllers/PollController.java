be266b9056520b631843870e5a71738ca2e410ab
mathieu.gamonnet <mathieu.gamonnet@atos.net>
2014-12-09 11:29:48 +0100

    Remove unused service classes

--- src/main/java/net/atos/entng/poll/controllers/PollController.java
+++ src/main/java/net/atos/entng/poll/controllers/PollController.java
@@ -1,14 +1,7 @@
 package net.atos.entng.poll.controllers;
 
-import java.util.Map;
-
-import net.atos.entng.poll.service.PollServiceImpl;
-
 import org.entcore.common.mongodb.MongoDbControllerHelper;
-import org.vertx.java.core.Vertx;
 import org.vertx.java.core.http.HttpServerRequest;
-import org.vertx.java.core.http.RouteMatcher;
-import org.vertx.java.platform.Container;
 
 import fr.wseduc.rs.ApiDoc;
 import fr.wseduc.rs.Delete;
@@ -23,88 +16,80 @@ import fr.wseduc.security.SecuredAction;
  * @author Atos
  */
 public class PollController extends MongoDbControllerHelper {
-	private String collection;
 
-	/**
-	 * Default constructor.
-	 * @param collection MongoDB collection to request.
-	 */
-	public PollController(String collection) {
-		super(collection);
-		this.collection = collection;
-	}
+    /**
+     * Default constructor.
+     * @param collection MongoDB collection to request.
+     */
+    public PollController(String collection) {
+        super(collection);
+    }
 
-	@Override
-	public void init(Vertx vertx, Container container, RouteMatcher rm, Map<String, fr.wseduc.webutils.security.SecuredAction> securedActions) {
-		super.init(vertx, container, rm, securedActions);
-		this.setCrudService(new PollServiceImpl(collection));
-	}
+    @Get("")
+    @SecuredAction("poll.view")
+    public void view(HttpServerRequest request) {
+        renderView(request);
+    }
 
-	@Get("")
-	@SecuredAction("poll.view")
-	public void view(HttpServerRequest request) {
-		renderView(request);
-	}
+    @Override
+    @Get("/list/all")
+    @ApiDoc("Allows to list all polls")
+    @SecuredAction("poll.list")
+    public void list(HttpServerRequest request) {
+        super.list(request);
+    }
 
-	@Override
-	@Get("/list/all")
-	@ApiDoc("Allows to list all polls")
-	@SecuredAction("poll.list")
-	public void list(HttpServerRequest request) {
-		super.list(request);
-	}
+    @Override
+    @Post("")
+    @ApiDoc("Allows to create a new poll")
+    @SecuredAction("poll.create")
+    public void create(HttpServerRequest request) {
+        super.create(request);
+    }
 
-	@Override
-	@Post("")
-	@ApiDoc("Allows to create a new poll")
-	@SecuredAction("poll.create")
-	public void create(HttpServerRequest request) {
-		super.create(request);
-	}
+    @Override
+    @Get("/:id")
+    @ApiDoc("Allows to get a poll associted to the given identifier")
+    @SecuredAction(value = "poll.read", type = ActionType.RESOURCE)
+    public void retrieve(HttpServerRequest request) {
+        super.retrieve(request);
+    }
 
-	@Override
-	@Get("/:id")
-	@ApiDoc("Allows to get a poll associted to the given identifier")
-	@SecuredAction(value = "poll.read", type = ActionType.RESOURCE)
-	public void retrieve(HttpServerRequest request) {
-		super.retrieve(request);
-	}
+    @Override
+    @Put("/:id")
+    @ApiDoc("Allows to update a poll associted to the given identifier")
+    @SecuredAction(value = "poll.manager", type = ActionType.RESOURCE)
+    public void update(HttpServerRequest request) {
+        super.update(request);
+    }
 
-	@Override
-	@Put("/:id")
-	@ApiDoc("Allows to update a poll associted to the given identifier")
-	@SecuredAction(value = "poll.manager", type = ActionType.RESOURCE)
-	public void update(HttpServerRequest request) {
-		super.update(request);
-	}
+    @Override
+    @Delete("/:id")
+    @ApiDoc("Allows to delete a poll associted to the given identifier")
+    @SecuredAction(value = "poll.manager", type = ActionType.RESOURCE)
+    public void delete(HttpServerRequest request) {
+        super.delete(request);
+    }
 
-	@Override
-	@Delete("/:id")
-	@ApiDoc("Allows to delete a poll associted to the given identifier")
-	@SecuredAction(value = "poll.manager", type = ActionType.RESOURCE)
-	public void delete(HttpServerRequest request) {
-		super.delete(request);
-	}
+    @Get("/share/json/:id")
+    @ApiDoc("Allows to get the current sharing of the poll given by its identifier")
+    @SecuredAction(value = "poll.manager", type = ActionType.RESOURCE)
+    public void share(HttpServerRequest request) {
+        shareJson(request, false);
+    }
 
-	@Get("/share/json/:id")
-	@ApiDoc("Allows to get the current sharing of the poll given by its identifier")
-	@SecuredAction(value = "poll.manager", type = ActionType.RESOURCE)
-	public void share(HttpServerRequest request) {
-		shareJson(request, false);
-	}
+    @Put("/share/json/:id")
+    @ApiDoc("Allows to update the current sharing of the poll given by its identifier")
+    @SecuredAction(value = "poll.manager", type = ActionType.RESOURCE)
+    public void sharePollSubmit(HttpServerRequest request) {
+        shareJsonSubmit(request, null);
+    }
 
-	@Put("/share/json/:id")
-	@ApiDoc("Allows to update the current sharing of the poll given by its identifier")
-	@SecuredAction(value = "poll.manager", type = ActionType.RESOURCE)
-	public void sharePollSubmit(HttpServerRequest request) {
-		shareJsonSubmit(request, null);
-	}
-
-	@Put("/share/remove/:id")
-	@ApiDoc("Allows to remove the current sharing of the poll given by its identifier")
-	@SecuredAction(value = "poll.manager", type = ActionType.RESOURCE)
-	public void removeSharePoll(HttpServerRequest request) {
-		removeShare(request, false);
-	}
+    @Put("/share/remove/:id")
+    @ApiDoc("Allows to remove the current sharing of the poll given by its identifier")
+    @SecuredAction(value = "poll.manager", type = ActionType.RESOURCE)
+    public void removeSharePoll(HttpServerRequest request) {
+        removeShare(request, false);
+    }
 
 }
